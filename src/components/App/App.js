043614d31ef9f62c643fd9edc1095784d7b31954
Matthew Renn <rennmatthewp@gmail.com>
2018-02-15 11:52:10 -0700

    fetches news and weather data in CDM connect App to redux store via connect dispatches actions to populate news and weather data in store

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
+import { populateNews, populateWeather } from '../../actions';
+import { nytKey, wuKey } from '../../helper/.apiKeys'
 
 class App extends Component {
+  componentDidMount() {
+    fetch(
+      `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${nytKey}`
+    )
+      .then(res => res.json())
+      .then(res => this.props.populateNews(res.results));
+    fetch(
+      `http://api.wunderground.com/api/${wuKey}/hourly/q/CO/Denver.json`
+    )
+      .then(res => res.json())
+      .then(res => this.props.populateWeather(res.hourly_forecast));
+  }
+
   render() {
-    return (
-      <div className="App">
-        Hello, World
-      </div>
-    );
+    return <div className="App">Hello, World</div>;
   }
 }
 
-export default App;
+export const mapDispatchToProps = dispatch => ({
+  populateNews: storiesArray => dispatch(populateNews(storiesArray)),
+  populateWeather: weatherArray => dispatch(populateWeather(weatherArray))
+});
+
+export default connect(null, mapDispatchToProps)(App);
