2935f5d156ab4e267a96e173b965f5cdaf49536d
Matthew Renn <rennmatthewp@gmail.com>
2018-02-18 14:48:40 -0700

    moves fetch requests into apiHelper.js sets fetch results in redux state in CDM in App

--- src/helper/apiHelper.js
+++ src/helper/apiHelper.js
@@ -1,10 +1,23 @@
-fetch(
-  `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${nytKey}`
-)
-  .then(res => res.json())
-  .then(res => this.props.populateNews(res.results));
-fetch(
-  `http://api.wunderground.com/api/${wuKey}/hourly/q/CO/Denver.json`
-)
-  .then(res => res.json())
-  .then(res => this.props.populateWeather(res.hourly_forecast));
+import { nytKey, wuKey } from './.apiKeys';
+
+export const getNews = () => {
+  return fetch(
+    `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${nytKey}`
+  )
+    .then(res => res.json())
+    .then(res => res.results)
+    .catch(error => {
+      throw new Error(error);
+    });
+};
+
+export const getWeather = () => {
+  return fetch(
+    `http://api.wunderground.com/api/${wuKey}/hourly/q/CO/Denver.json`
+  )
+    .then(res => res.json())
+    .then(res => res.hourly_forecast)
+    .catch(error => {
+      throw new Error(error);
+    });
+};
