71ec43bc023114c92b8261822c7c5172d32512d6
Matthew Renn <rennmatthewp@gmail.com>
2018-02-21 11:06:11 -0700

    adds caption to image alt, adds button, adds caption to proptypes

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const NewsCard = ({ title, abstract, byline, url, thumbnail }) => {
+export const NewsCard = ({
+  title,
+  abstract,
+  byline,
+  url,
+  thumbnail,
+  caption
+}) => {
   return (
     <div>
       <a href={url}>
-        <img src={thumbnail}/>
+        <img src={thumbnail} alt={caption} />
         <h3>{title}</h3>
       </a>
+      <button>O</button>
       <h5>{byline}</h5>
       <p>{abstract}</p>
-
     </div>
   );
 };
@@ -20,7 +27,8 @@ NewsCard.propTypes = {
   abstract: PropTypes.string,
   byline: PropTypes.string,
   url: PropTypes.string,
-  thumbnail: PropTypes.string
+  thumbnail: PropTypes.string,
+  caption: PropTypes.string
 };
 
 export default NewsCard;
