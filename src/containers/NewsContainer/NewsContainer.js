8b3ebbc3a2ea7c38dade7385b20bea3f273025fb
Matthew Renn <rennmatthewp@gmail.com>
2018-02-24 14:23:53 -0700

    gets news data and sets it in redux store

--- src/containers/NewsContainer/NewsContainer.js
+++ src/containers/NewsContainer/NewsContainer.js
@@ -1,24 +1,68 @@
 import React, { Component } from 'react';
-import {arrayOf, object} from 'prop-types';
+import { arrayOf, object, func } from 'prop-types';
 import { connect } from 'react-redux';
+import { NewsNav } from '../../components/NewsNav/NewsNav';
 import NewsCard from '../../components/NewsCard/NewsCard';
+import { getNewsData, cleanNewsData } from '../../helper/apiCalls';
+import { populateNews } from '../../actions';
 import './NewsContainer.css';
 
 export class NewsContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      errorStatus: null
+    };
+  }
+
+  componentDidMount() {
+    this.getNews();
+  }
+
+  getNews = async (section) => {
+    try {
+      const newsResponse = await getNewsData(section);
+      const cleanedNewsData = await cleanNewsData(newsResponse.results);
+      this.props.populateNews(cleanedNewsData);
+    } catch (error) {
+      this.setState({ errorStatus: error });
+    }
+  };
+
+  selectSection = event => {
+    const { name } = event.target;
+    this.getNews(name);
+  };
+
   render() {
     const articles = this.props.articles.map(article => (
       <NewsCard key={article.title} {...article} />
     ));
-    return <div className="news-container">{articles}</div>;
+
+    if (this.state.errorStatus) {
+      return <div>Error Fetching News</div>;
+    }
+
+    return (
+      <div className="news-container">
+        <NewsNav selectSection={this.selectSection} />
+        {articles}
+      </div>
+    );
   }
 }
 
 NewsContainer.propTypes = {
-  articles: arrayOf(object)
+  articles: arrayOf(object),
+  populateNews: func
 };
 
 export const mapStateToProps = state => ({
   articles: state.newsStories
 });
 
-export default connect(mapStateToProps, null)(NewsContainer);
+export const mapDispatchToProps = dispatch => ({
+  populateNews: stories => dispatch(populateNews(stories))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewsContainer);
