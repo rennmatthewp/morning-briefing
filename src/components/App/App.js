1784ecc5ba04aa57a2f47d31fff88ddb858c0507
Matthew Renn <rennmatthewp@gmail.com>
2018-02-24 14:18:57 -0700

    pulls news fetch out of App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import './App.css';
 import { connect } from 'react-redux';
-import { populateNews, populateWeather } from '../../actions';
-import {
-  getNewsData,
-  getWeatherData,
-  cleanNewsData,
-  cleanWeatherData
-} from '../../helper/apiCalls';
+import { populateWeather } from '../../actions';
+import { getWeatherData, cleanWeatherData } from '../../helper/apiCalls';
 import NewsContainer from '../../containers/NewsContainer/NewsContainer';
 //eslint-disable-next-line
 import WeatherContainer from '../../containers/WeatherContainer/WeatherContainer';
+import './App.css';
 
 export class App extends Component {
   constructor() {
@@ -23,18 +18,7 @@ export class App extends Component {
 
   componentDidMount() {
     this.getWeather();
-    this.getNews();
   }
-
-  getNews = async () => {
-    try {
-      const newsResponse = await getNewsData();
-      const cleanedNewsData = await cleanNewsData(newsResponse.results);
-      this.props.populateNews(cleanedNewsData);
-    } catch (error) {
-      this.setState({ errorStatus: error });
-    }
-  };
 
   getWeather = async () => {
     try {
@@ -60,12 +44,10 @@ export class App extends Component {
 }
 
 App.propTypes = {
-  populateNews: PropTypes.func,
   populateWeather: PropTypes.func
 };
 
 export const mapDispatchToProps = dispatch => ({
-  populateNews: stories => dispatch(populateNews(stories)),
   populateWeather: weather => dispatch(populateWeather(weather))
 });
 
