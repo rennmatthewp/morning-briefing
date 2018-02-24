2233bdbf1af8541d8e56fa7a19a2f735156822c9
Matthew Renn <rennmatthewp@gmail.com>
2018-02-24 16:36:48 -0700

    adds dynamiclly rendered buttons for desired news sections

--- src/components/NewsNav/NewsNav.js
+++ src/components/NewsNav/NewsNav.js
@@ -2,22 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const NewsNav = ({ selectSection }) => {
-  return (
-    <div>
-      <button name="home" onClick={selectSection}>
-        Top Stories
-      </button>
-      <button name="technology" onClick={selectSection}>
-        Technology
-      </button>
-      <button name="fashion" onClick={selectSection}>
-        Fashion
-      </button>
-      <button name="business" onClick={selectSection}>
-        Business
-      </button>
-    </div>
-  );
+  const newsSections = {
+    home: 'Home',
+    world: 'World',
+    national: 'National',
+    nyregion: 'NY Region',
+    politics: 'Politics',
+    technology: 'Technology',
+    science: 'Science',
+    business: 'Business',
+    opinion: 'Opinion',
+    tmagazine: 'NYT Magazine'
+  };
+
+  const sectionButtons = Object.keys(newsSections).map(key => (
+    <button name={key} onClick={selectSection}>
+      {newsSections[key]}
+    </button>
+  ));
+  
+  return <div>{sectionButtons}</div>
 };
 
 NewsNav.propTypes = {
