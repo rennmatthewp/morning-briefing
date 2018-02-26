30a0c9116dc2c740672897fd195fb831fd2577b3
Matthew Renn <rennmatthewp@gmail.com>
2018-02-26 03:24:08 -0700

    changes buttons to NavLinks for active styling and routing moves newsSections object in to apiCalls file

--- src/components/NewsNav/NewsNav.js
+++ src/components/NewsNav/NewsNav.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+import { newsSections } from '../../helper/apiCalls';
+import './NewsNav.css';
 
 export const NewsNav = ({ selectSection }) => {
-  const newsSections = {
-    home: 'Home',
-    world: 'World',
-    national: 'National',
-    nyregion: 'NY Region',
-    politics: 'Politics',
-    technology: 'Technology',
-    science: 'Science',
-    business: 'Business',
-    opinion: 'Opinion',
-    tmagazine: 'NYT Magazine'
-  };
+  const sectionButtons = Object.keys(newsSections).map(section => {
+    return (
+      <NavLink
+        to={`/${section}`}
+        name={section}
+        key={section}
+        onClick={selectSection}
+        activeClassName="selected">
+        {newsSections[section]}
+      </NavLink>
+    );
+  });
 
-  const sectionButtons = Object.keys(newsSections).map(key => (
-    <button name={key} onClick={selectSection}>
-      {newsSections[key]}
-    </button>
-  ));
-  
-  return <div>{sectionButtons}</div>
+  return <div>{sectionButtons}</div>;
 };
 
 NewsNav.propTypes = {
