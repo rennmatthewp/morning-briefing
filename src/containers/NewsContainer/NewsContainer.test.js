510ab73133bf9b9c2e3a3d5b45996984403786ac
Matthew Renn <rennmatthewp@gmail.com>
2018-02-26 11:41:25 -0700

    adds MDTP test for NewsContainer

--- src/containers/NewsContainer/NewsContainer.test.js
+++ src/containers/NewsContainer/NewsContainer.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { NewsContainer, mapStateToProps } from './NewsContainer';
+import {
+  NewsContainer,
+  mapStateToProps,
+  mapDispatchToProps
+} from './NewsContainer';
 import { shallow } from 'enzyme';
 import { expectedNewsObj } from '../../mockData';
 
 describe('NewsContainer', () => {
   it('should match the snapshot', () => {
+    const mockPopulateNews = jest.fn();
     const renderedNewsContainer = shallow(
       <NewsContainer newsStories={expectedNewsObj} />
     );
@@ -15,5 +20,13 @@ describe('NewsContainer', () => {
     const mapped = mapStateToProps({ newsStories: expectedNewsObj });
 
     expect(mapped.newsStories).toEqual(expectedNewsObj);
+  });
+
+  it('should call the dispatch fn when calling a fn from MDTP', () => {
+    const mockDispatch = jest.fn();
+    const mapped = mapDispatchToProps(mockDispatch);
+
+    mapped.populateNews();
+    expect(mockDispatch).toHaveBeenCalled();
   });
 });
