2059d3997f712736ab8d75ae0acd9e86a7355acc
Matthew Renn <rennmatthewp@gmail.com>
2018-03-01 08:54:34 -0700

    pulls routing logic into App render returns a Switch and Redirect to '/home/hourly' adds logic to handle Redirect case passes dynamic route to Containers

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import NewsContainer from '../../containers/NewsContainer/NewsContainer';
 //eslint-disable-next-line
 import WeatherContainer from '../../containers/WeatherContainer/WeatherContainer';
+import { newsSections } from '../../helper/apiCalls';
 import './App.css';
 
 export class App extends Component {
+  validateRoute = ({ section, category }) => {
+    if (category !== 'hourly' && category !== 'daily') return false;
+    if (!newsSections[section]) return false;
+    return true;
+  };
+
   render() {
     return (
       <div className="app">
-        <header>
-          <h1>Morning Briefing</h1>
-          <Route path="/" component={WeatherContainer} />
-        </header>
-        <Route path="/" component={NewsContainer} />
+        <Switch>
+          <Route
+            path="/:section/:category"
+            render={({ match }) => {
+              return this.validateRoute(match.params) ? (
+                <div>
+                  <header>
+                    <h1>Morning Briefing</h1>
+                    <WeatherContainer section={match.params.section} />
+                  </header>
+                  <NewsContainer category={match.params.category} />
+                </div>
+              ) : (
+                <Redirect to="/home/hourly" />
+              );
+            }}
+          />
+          <Redirect from="*" to="/home/hourly" />
+        </Switch>
       </div>
     );
   }
